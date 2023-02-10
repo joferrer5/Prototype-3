16aeb16b36ca227a242865a017798dc6fc15500b
joferrer5 <joferrer@s.sfusd.edu>
2023-02-10 15:23:12 -0800

    3.1 part 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody playerRB;
-
+    public float jumpForce;
+    public float gravityModifier;
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            playerRB.AddForce(Vector3.up * 100, ForceMode.Impulse); 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
         }
     }
 }
