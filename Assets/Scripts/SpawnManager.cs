850995d5b59829c00e375732635c3cbd5e929fa1
joferrer5 <joferrer@s.sfusd.edu>
2023-02-13 14:26:22 -0800

    3.1 part 8

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    void Start()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
