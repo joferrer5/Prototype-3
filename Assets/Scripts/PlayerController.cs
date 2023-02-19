3c151adff3f5e139c242b66f384e87c072e91d0f
joferrer5 <121971176+joferrer5@users.noreply.github.com>
2023-02-18 17:45:22 -0800

    3.4 part 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,13 +12,14 @@ public class PlayerController : MonoBehaviour
     private Animator playerAnim;
     public AudioClip jumpSound;
     public AudioClip crashSound;
+    private AudioSource playerAudio;
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
-        
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,6 +32,7 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     
     }
@@ -51,6 +53,7 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
