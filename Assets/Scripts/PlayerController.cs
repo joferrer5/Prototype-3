bc43511a137c699606d6488402f32fa23792a0ea
joferrer5 <121971176+joferrer5@users.noreply.github.com>
2023-02-18 17:00:48 -0800

    3.3 part 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -41,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
